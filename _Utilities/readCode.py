4f18132c9cc44a8e1fa293e6797cd5451ce15880
JoanaFigPinto <joanapinto@Joanas-MBP.Home>
2021-02-19 16:34:13 +0000

    VScode IDE addition into C10

--- _Utilities/readCode.py
+++ _Utilities/readCode.py
@@ -6,46 +6,48 @@ This script allows the automatic collection of python code from formatted notebo
 An example is provided.
 
 '''
+class pythonScript():
+    def collectPythonCode (self, notebook=".ipynb"):
+        """ 
+        Open formatted scientIST notebook and retrieves a python script.
+        """
+        try:
+            f = open(notebook,"r")
+            print('i')
+        except:
+            notebook=notebook+'.ipynb'
+            f = open(notebook,"r")
 
-def collectPythonCode (notebook=".ipynb"):
-    """ 
-    Open formatted scientIST notebook and retrieves a python script.
-    """
-    try:
-        f = open(notebook,"r")
-        print('i')
-    except:
-        notebook=notebook+'.ipynb'
-        f = open(notebook,"r")
+        # open and read notebook (json object)
+        data = f.read()
+        jsonObj = json.loads(data)
 
-    # open and read notebook (json object)
-    data = f.read()
-    jsonObj = json.loads(data)
+        keylist = jsonObj.keys()
+        itemslist = jsonObj.items()
 
-    keylist = jsonObj.keys()
-    itemslist = jsonObj.items()
+        f.seek(0)        
+    
+        python_file=open("script_"+str(notebook[0:-6])+".py","w")
 
-    f.seek(0)        
-   
-    python_file=open("script_"+str(notebook[0:-6])+".py","w")
+        # walks through the cells of the notebook
+        length=len(jsonObj['cells'])
+        for i in range(0,length):
+            dict_=dict(jsonObj['cells'][i])
+            dict_keys=dict_.keys()
+            if dict_['cell_type']=='code':    
+                python_file.write("\n")        
+                len_cell=len(dict_['source'])
+                for j in range(0,len_cell):
+                    print(str(dict_['source'][j]))
+                    print('j')
+                    if str(dict_['source'][j]).startswith('!pip install') or str(dict_['source'][j]).startswith('%matplotlib notebook') or str(dict_['source'][j]).startswith('mpld3.enable_notebook()'):
+                        python_file.write('#'+str(dict_['source'][j]))
+                    else:
+                        python_file.write(str(dict_['source'][j]))
+        python_file.close()
 
-    # walks through the cells of the notebook
-    length=len(jsonObj['cells'])
-    for i in range(0,length):
-        dict_=dict(jsonObj['cells'][i])
-        dict_keys=dict_.keys()
-        if dict_['cell_type']=='code':    
-            python_file.write("\n")        
-            len_cell=len(dict_['source'])
-            for j in range(0,len_cell):
-                print(str(dict_['source'][j]))
-                print('j')
-                if str(dict_['source'][j]).startswith('!pip install') or str(dict_['source'][j]).startswith('%matplotlib notebook') or str(dict_['source'][j]).startswith('mpld3.enable_notebook()'):
-                    python_file.write('#'+str(dict_['source'][j]))
-                else:
-                    python_file.write(str(dict_['source'][j]))
-    python_file.close()
-
+if __name__ == 'main':
+    pythonScript()
 
 ## pip install como comentário
 ## excluir mlp3
@@ -53,4 +55,4 @@ def collectPythonCode (notebook=".ipynb"):
 # por dentro dos utilities
 
 # EXAMPLE
-collectPythonCode(notebook="F001 Swimming.ipynb")
+#collectPythonCode(notebook="F001 Swimming.ipynb")
