1920797369d3081673f21c87a9fcd539ed6b9ea2
JoanaFigPinto <jjoana.pinto@hotmail.com>
2021-03-23 16:52:42 +0000

    new nb DAC

--- _Utilities/readCode.py
+++ _Utilities/readCode.py
@@ -1,5 +1,6 @@
 import json
 import os
+
 """
 This script allows the automatic collection of python code from ScientIST notebooks.
 
@@ -36,9 +37,7 @@ def collectPythonCode (notebook=".ipynb",folder=""):
 
     keylist = jsonObj.keys()
     itemslist = jsonObj.items()
-
     f.seek(0)        
-
     python_file=open("script_"+str(notebook[0:-6])+".py","w")
 
     # walks through the cells of the notebook
@@ -60,6 +59,126 @@ def collectPythonCode (notebook=".ipynb",folder=""):
 
     return python_file
 
+def creatNB(NBskeleton='NBskeleton.txt', nameNB="test"):
+    """
+    Opens python script and retrieves a notebook file containing the code collected.
+
+    Parameters
+    ----------
+    NBskeleton: .txt
+        .txt file containing the styling used in a ScientIST Notebook.
+
+    nameNB: str
+        Name to be given to a new .ipynb file.
+    
+    Returns
+    -------
+    NB: str
+        .ipynb file in accordance with the ScientIST Notebook styling.
+    """
+
+    f=open(nameNB + '.ipynb', "w")
+    NBskeleton=open(NBskeleton,"r")
+    dataNB = NBskeleton.read()
+    f.write(dataNB)
+    nameNB+='.ipynb'
+
+    f.close()
+    NB = nameNB
+
+    return NB
+
+
+
+def createNBfromScript (nameNB="test", script="../../ECG.py"):
+    """
+    Opens python script and retrieves a notebook file containing the code collected.
+
+    Parameters
+    ----------
+    nameNB: str
+        Name to be given to a new .ipynb file.
+
+    script: str
+        Python script to collect code from.
+    
+    Returns
+    -------
+    ipynb_file: 
+        Notebook file containing the code collected in the original python script.
+    """
+    
+    if nameNB.endswith('.ipynb'):
+        name_=nameNB[:-6]
+    else:
+        name_=nameNB
+        
+    name_=name_+'notebook'
+    creatNB(nameNB=name_)  
+
+    # opens NB
+    name_nb=name_+'.ipynb'
+    f = open(name_nb,"r")
+    fr = f.read()
+    
+    jsonNB = json.loads(fr)
+    lastCells = jsonNB['cells'][-7:]
+    jsonNB['cells']=jsonNB['cells'][:-7]
+    length=len(jsonNB['cells'])
+
+    f_name=script
+    if f_name.endswith('.py'):
+        fileObj = open(f_name, 'r')
+        data=fileObj.read()
+        s=data
+
+        # find global script description
+        p='"""'
+        i=s.find(p)
+        i2_ = s.find('"""', i+3)
+        global_description=data[i:i2_]
+        jsonNB['cells'].append({"cell_type": "markdown", "metadata": {}, "source": [ global_description[3:]]})
+
+        
+        # find functions
+        p="def"
+        i1 = s.find(p,0)
+        i2 = s.find("(", i1+1)
+        i3= s.find("def", i1+1)
+        function_name=s[i1:i2]
+        function=s[i1:i3]
+        jsonNB['cells'].append({"cell_type": "markdown", "metadata": {}, "source": [ function_name[3:]]})
+        jsonNB['cells'].append({"cell_type": "code", "execution_count": 1, "metadata": {}, "outputs": [], "source": [function] })
+        
+        while i1 != -1:
+            i1 = s.find(p, i2+1)
+            i2 = s.find("(", i1+1)
+            function_name=s[i1:i2]
+            
+            if function_name[3:] != '':
+                # finds description of the function
+                p_='"""'
+                i1_=s.find(p_, i2)
+                i2_ = s.find('"""', i1_+3)
+                global_description=s[i1_:i2_]
+                i3= s.find(p, i1+1)
+                function=s[i1:i3]
+                jsonNB['cells'].append({"cell_type": "code", "execution_count": 1, "metadata": {}, "outputs": [], "source": [function] })
+
+    for cell in lastCells:
+        jsonNB['cells'].append(cell)
+
+    f.seek(0) 
+    with open(name_nb, 'w') as json_data:
+        json.dump(jsonNB, json_data)
+
+    ipynb_file= jsonNB
+
+    return ipynb_file
+
 
 # EXAMPLE
-collectPythonCode(notebook="F001 Swimming.ipynb",folder="../F.Applications/")
+#collectPythonCode(notebook="F001 Swimming.ipynb",folder="../F.Applications/")
+
+#creatNB()    
+createNBfromScript(nameNB="ECG", script="../../ECG.py")
